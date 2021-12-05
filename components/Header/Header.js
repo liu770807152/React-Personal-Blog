3db432f0788da1ab6cb2f856ecacac9962767ec3
Bradley <liu770807152@hotmail.com>
2021-12-06 01:12:22 +1100

    fix small problems

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -11,21 +11,23 @@ const Header = () => (
 	<div className={styles.header}>
 		<Row type='flex' justify='center'>
 			<Col xs={24} sm={24} md={10} lg={16} xl={12}>
-				<span className={styles.header__logo}>Chris Blog</span>
-				<span className={styles.header__text}>Never settle.</span>
+				<div className={styles.header__title}>
+					<span className={styles.header__logo}>Chris Blog</span>
+					<span className={styles.header__text}>Never settle.</span>
+				</div>
 			</Col>
 			<Col xs={0} sm={0} md={14} lg={8} xl={6}>
-				<Menu mode='horizontal'>
+				<Menu mode='horizontal' className={styles.menu}>
 					<Menu.Item key='home'>
-						<HomeOutlined style={{ fontSize: '1rem' }} />
+						<HomeOutlined style={{ fontSize: '1.2rem' }} />
 						<span className={styles['menu-item__text']}>HOME</span>
 					</Menu.Item>
 					<Menu.Item key='video'>
-						<YoutubeOutlined style={{ fontSize: '1rem' }} />
+						<YoutubeOutlined style={{ fontSize: '1.2rem' }} />
 						<span className={styles['menu-item__text']}>VIDEO</span>
 					</Menu.Item>
 					<Menu.Item key='life'>
-						<SmileOutlined style={{ fontSize: '1rem' }} />
+						<SmileOutlined style={{ fontSize: '1.2rem' }} />
 						<span className={styles['menu-item__text']}>LIFE</span>
 					</Menu.Item>
 				</Menu>
