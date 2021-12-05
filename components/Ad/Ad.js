1db98774fe30b608e4f9257ea38660483bcd4259
Bradley <liu770807152@hotmail.com>
2021-12-05 22:50:18 +1100

    add modules of advertisement & footer

---
+++ components/Ad/Ad.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Image from 'next/image';
+import classNames from 'classnames';
+import styles from './ad.module.css';
+
+const Ad = () => (
+	<div
+		className={classNames({ [styles.ad__container]: true, comm__box: true })}
+	>
+		<div className={styles.ad__wrapper}>
+			<Image
+				src='/Saito-Asuka.jpg'
+				width='100%'
+				height='100%'
+				layout='responsive'
+				objectFit='cover'
+				alt='Asuka'
+			/>
+		</div>
+	</div>
+);
+
+export default Ad;
