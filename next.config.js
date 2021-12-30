f390bd16ea3f32c3cbeee05f13c6a70f0f1e793c
Bradley <liu770807152@hotmail.com>
2021-12-31 01:45:59 +1100

    add support of styled components and sass

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const withCss = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
 
 if (typeof require !== 'undefined') {
-	require.extensions['.css'] = (file) => {};
+  require.extensions['.css'] = (file) => {};
 }
 
-module.exports = withCss({});
+module.exports = withSass({});
 
 module.exports = {
-	reactStrictMode: true,
+  reactStrictMode: true
 };
