6f8cd4531c8e63503f112d994b2a3310a3fa58b1
Bradley <liu770807152@hotmail.com>
2022-01-13 23:41:50 +1100

    adjust API routes

--- pages/api/article/[id]/index.ts
+++ pages/api/article/[id]/index.ts
@@ -1,5 +1,5 @@
-import request from '../../../utils/fetcher';
-import path from '../../../utils/apiUrl';
+import request from '../../../../utils/fetcher';
+import path from '../../../../utils/apiUrl';
 
 export async function handler({ query: { id } }, res) {
   return request({
