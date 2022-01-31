2818f5970d75439193144c35cc699b328a447305
Bradley <liu770807152@hotmail.com>
2022-01-31 16:36:57 +1100

    improve Footer & make it responsive

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import footerData from '../../utils/footerData';
 import styles from './footer.module.css';
 
-const Footer: React.FC = () => (
-  <div className={styles.footer__container}>
-    <div>
-      <span>Created with React Hooks & Node & Ant Design</span>
-    </div>
-  </div>
-);
+const Footer: React.FC = () => {
+  const [active, setActive] = useState(null);
+
+  return (
+    <footer className="footer">
+      <div className="footer__logo-container">
+        <div className="footer__logo-container--left">
+          <a href="/">
+            <img className="logo" src="/logo_transparent.png" alt="logo"></img>
+          </a>
+        </div>
+        <div className="footer__logo-container--right">
+          <div>
+            <span>Chris Liu</span>
+          </div>
+          <div>
+            <span>Copyright © 2022</span>
+          </div>
+        </div>
+      </div>
+      <div className="footer__icon-container">
+        <div className="footer__icon-container--inner">
+          <div className="footer__icon-container--list">
+            {footerData.map((item) => (
+              <div
+                className="footer__icon-container--item"
+                onMouseEnter={() => setActive(item.id)}
+                onMouseLeave={() => setActive(null)}
+                key={item.id}
+                style={{
+                  backgroundColor: `${active === item.id ? item.bgColor : ''}`,
+                }}
+              >
+                <i
+                  className="footer__icon-container--icon"
+                  style={{
+                    backgroundImage: `url(${item.icon})`,
+                  }}
+                />
+                <div className="footer__icon-container--item-desc">
+                  <div className="footer__icon-container--item-hide-desc"></div>
+                  <div
+                    className="footer__icon-container--item-desc-inner"
+                    style={item.bgColor === '#fff' ? { color: '#333' } : null}
+                  >
+                    <p className="footer__icon-container--name">{item.name}</p>
+                    <p className="footer__icon-container--describe">{item.description}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="footer__text-container">
+        <p>Actively seeking a full-time position of software engineer</p>
+        <p>Contact me</p>
+        <p>icons</p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
